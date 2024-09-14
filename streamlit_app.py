6aedf5400c3aec6e050989539ef009c3f59bbef5
kostia-s920 <kostia.s920@gmail.com>
2024-09-14 19:56:17 +0300

    Перевіряємо, що дати коректно перетворені на datetime

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ def plot_keyword_history(df, keyword, selected_url, chart_type):
         st.write(f"No data for URL: {selected_url}")
         return
 
-    # Перевіряємо, що дати коректно перетворені на datetime
+    # Перевіряємо  що дати коректно перетворені на datetime
     url_data['date_checked'] = pd.to_datetime(url_data['date_checked'], errors='coerce')
 
     # Використовуємо функцію для витягання кількості ключових слів
