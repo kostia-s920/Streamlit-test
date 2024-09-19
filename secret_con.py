4b0944fc614b18741b12bb376af02386c9578a92
kostia-s920 <kostia.s920@gmail.com>
2024-09-19 10:35:32 +0300

    тест підключення 4

--- secret_con.py
+++ secret_con.py
@@ -1,37 +1,36 @@
-import streamlit as st
+import os
 import base64
+import streamlit as st
 
-# Отримуємо секрети з streamlit secrets
-DB_USERNAME = st.secrets["DB_USERNAME"]
-DB_PASSWORD = st.secrets["DB_PASSWORD"]
-DB_HOST = st.secrets["DB_HOST"]
-DB_PORT = st.secrets["DB_PORT"]
-DB_NAME = st.secrets["DB_NAME"]
-SSL_MODE = st.secrets["SSL_MODE"]
-DB_SSL_ROOT_CERT = st.secrets["DB_SSL_ROOT_CERT"]
+# Отримуємо секрети
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+SSL_MODE = os.getenv('SSL_MODE')
+DB_SSL_ROOT_CERT_BASE64 = os.getenv('DB_SSL_ROOT_CERT')  # Переконайтеся, що ключ в secrets правильний
 
 # Декодуємо SSL сертифікат
-ssl_cert_decoded = base64.b64decode(DB_SSL_ROOT_CERT)
+ssl_cert_decoded = base64.b64decode(DB_SSL_ROOT_CERT_BASE64)
 
 # Виведення значень для тестування
-def test_secrets():
-    st.write("DB_USERNAME:", DB_USERNAME)
-    st.write("DB_PASSWORD:", DB_PASSWORD)
-    st.write("DB_HOST:", DB_HOST)
-    st.write("DB_NAME:", DB_NAME)
-    st.write("DB_PORT:", DB_PORT)
-    st.write("SSL_MODE:", SSL_MODE)
+st.write("DB_USERNAME:", DB_USERNAME)
+st.write("DB_PASSWORD:", DB_PASSWORD)
+st.write("DB_HOST:", DB_HOST)
+st.write("DB_NAME:", DB_NAME)
+st.write("DB_PORT:", DB_PORT)
+st.write("SSL_MODE:", SSL_MODE)
 
-    # Тест декодування сертифікату з Base64
-    try:
-        ssl_cert_decoded = base64.b64decode(DB_SSL_ROOT_CERT)
-        st.write("SSL Root Certificate decoded successfully!")
-        # Виведемо перші 100 символів сертифікату для підтвердження
-        st.write(ssl_cert_decoded[:100])
-    except Exception as e:
-        st.write("Error decoding SSL Root Certificate:", str(e))
+# Тест декодування сертифікату з Base64
+try:
+    ssl_cert_content = base64.b64decode(DB_SSL_ROOT_CERT_BASE64)  # Інша змінна
+    st.write("SSL Root Certificate decoded successfully!")
+    # Виведемо перші 100 символів сертифікату для підтвердження
+    st.write(ssl_cert_content[:100])
+except Exception as e:
+    st.write("Error decoding SSL Root Certificate:", str(e))
 
 # Запустити функцію тестування
 if __name__ == "__main__":
     st.title("Test Streamlit Secrets")
-    test_secrets()
