7b9603937ce06b8c1d7a6806015e04e83726b27e
kostia-s920 <kostia.s920@gmail.com>
2024-09-19 10:22:10 +0300

    тест підключення 1

---
+++ secret_con.py
@@ -0,0 +1,35 @@
+import os
+import base64
+import streamlit as st
+
+def test_secrets():
+    # Отримання секретів із середовища
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_PORT = os.getenv('DB_PORT')
+    SSL_MODE = os.getenv('SSL_MODE')
+    DB_SSL_ROOT_CERT_BASE64 = os.getenv('DB_SSL_ROOT_CERT')
+
+    # Виведення значень для тестування
+    st.write("DB_USERNAME:", DB_USERNAME)
+    st.write("DB_PASSWORD:", DB_PASSWORD)
+    st.write("DB_HOST:", DB_HOST)
+    st.write("DB_NAME:", DB_NAME)
+    st.write("DB_PORT:", DB_PORT)
+    st.write("SSL_MODE:", SSL_MODE)
+
+    # Тест декодування сертифікату з Base64
+    try:
+        ssl_cert_decoded = base64.b64decode(DB_SSL_ROOT_CERT_BASE64)
+        st.write("SSL Root Certificate decoded successfully!")
+        # Виведемо перші 100 символів сертифікату для підтвердження
+        st.write(ssl_cert_decoded[:100])
+    except Exception as e:
+        st.write("Error decoding SSL Root Certificate:", str(e))
+
+# Запустити функцію тестування
+if __name__ == "__main__":
+    st.title("Test Streamlit Secrets")
+    test_secrets()
